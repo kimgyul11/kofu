e7c779b7dd75056c57f310c134743b6b2e47fcf2
kimgyul11 <coloo88@naver.com>
2022-11-02 16:14:40 +0900

    commit

--- src/main/java/com/kofu/infra/modules/home/HomeController.java
+++ src/main/java/com/kofu/infra/modules/home/HomeController.java
@@ -1,7 +1,14 @@
 package com.kofu.infra.modules.home;
 
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -9,6 +16,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 
 
@@ -126,6 +135,83 @@ public class HomeController {
 		service.answerSelect(dto);
 		redirectAttributes.addFlashAttribute("vo",vo);
 		return "redirect:/queview";
+	}
+	//-------------------------api테스트----------------------
+	@RequestMapping(value = "/test/publicCorona1List")
+	public String publicCorona1List(Model model) throws Exception {
+		
+		System.out.println("asdfasdfasdf");
+		
+		String apiUrl = "http://apis.data.go.kr/B551177/StatusOfPassengerWorldWeatherInfo/getPassengerArrivalsWorldWeather?serviceKey=t4NbJEljOJc15d9vAm5T1cdsT3wG5o8PYQB%2F%2FNrRfKNcKmNQHCLhlqzf9O6rKmhdXG67a4B2YLayH7sjO%2BOCTQ%3D%3D&numOfRows=10&pageNo=1&from_time=0000&to_time=2400&lang=K&type=json";
+		
+		URL url = new URL(apiUrl);
+		HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+		httpURLConnection.setRequestMethod("GET");
+		
+		BufferedReader bufferedReader;
+		if (httpURLConnection.getResponseCode() >= 200 && httpURLConnection.getResponseCode() <= 300) {
+			bufferedReader = new BufferedReader(new InputStreamReader(httpURLConnection.getInputStream()));
+		} else {
+			bufferedReader = new BufferedReader(new InputStreamReader(httpURLConnection.getErrorStream()));
+		}
+		
+		StringBuilder stringBuilder = new StringBuilder();
+		String line;
+		while ((line = bufferedReader.readLine()) != null) {
+			System.out.println("line: " + line);
+			stringBuilder.append(line);
+		}
+
+		bufferedReader.close();
+		httpURLConnection.disconnect();
+
+		System.out.println("stringBuilder.toString(): " + stringBuilder.toString());
+		
+//		json object + array string -> java map
+		
+        ObjectMapper objectMapper = new ObjectMapper();
+        Map<String, Object> map = objectMapper.readValue(stringBuilder.toString(), Map.class);
+        
+		Map<String, Object> response = new HashMap<String, Object>();
+		response = (Map<String, Object>) map.get("response");
+		
+        System.out.println("######## Map");
+		for (String key : response.keySet()) {
+			String value = String.valueOf(response.get(key));	// ok
+			System.out.println("[key]:" + key + ", [value]:" + value);
+		}
+
+		Map<String, Object> header = new HashMap<String, Object>();
+		header = (Map<String, Object>) response.get("header");
+		
+		System.out.println("######## Header");
+		for (String key : header.keySet()) {
+			String value = String.valueOf(header.get(key));	// ok
+			System.out.println("[key]:" + key + ", [value]:" + value);
+		}
+		
+//		String aaa = (String) header.get("resultCode");
+		
+//		System.out.println("header.get(\"resultCode\"): " + header.get("resultCode"));
+//		System.out.println("header.get(\"resultMsg\"): " + header.get("resultMsg"));
+		
+		Map<String, Object> body = new HashMap<String, Object>();
+		body = (Map<String, Object>) response.get("body");
+		
+		List<Home> items = new ArrayList<Home>();
+		items = (List<Home>) body.get("items");
+		
+		
+		System.out.println("items.size(): " + items.size());
+		
+//		for(Home item : items) {
+//			System.out.println(item.getMM());
+//		}
+		
+		model.addAllAttributes(header);
+		model.addAllAttributes(body);
+		
+		return "infra/test/publicCorona1List";
 	}
 
 }
